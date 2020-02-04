62f777384c5a0c1526712f0f4b3acb78cf2378dc
Valentin Fries <contact@fries.io>
2020-02-04 16:50:10 +0100

    FIX - age_set containsExactlyInAnyOrder

--- src/test/java/io/fries/koans/stream/Exercise05.java
+++ src/test/java/io/fries/koans/stream/Exercise05.java
@@ -37,7 +37,7 @@ class Exercise05 extends OnlineStore {
         Set<Integer> ageSet = null;
 
         assertThat(ageSet).hasSize(9);
-        assertThat(ageSet).containsExactly(21, 22, 26, 27, 28, 32, 35, 36, 38);
+        assertThat(ageSet).containsExactlyInAnyOrder(21, 22, 26, 27, 28, 32, 35, 36, 38);
     }
 
     /**
